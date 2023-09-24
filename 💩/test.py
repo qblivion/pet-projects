dc989becaa44159d9134ba2aa80fa131c346521a
qblivion <71896680+qblivion@users.noreply.github.com>
2023-09-25 00:21:30 +0300

    Update test.py

--- 💩/test.py
+++ 💩/test.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 import asyncio
 
-api_id = 19890531
-api_hash = "7ec8158285a1d583eaea1d881c70ca2e"
+api_id = 0          # Поменяйте 
+api_hash = "7ec81"  # Эти параметры
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
 
@@ -10,7 +10,7 @@ async def react_to_messages(chat_id, emoji):
     async with Client("my_account", api_id, api_hash) as app:
         async for message in app.get_chat_history(chat_id, limit=100):
             await app.send_reaction(chat_id, message.id, emoji)
-            # await asyncio.sleep(1)
+            await asyncio.sleep(1)
 
 
-asyncio.run(react_to_messages("@anonimus2004", "💩"))
+asyncio.run(react_to_messages("И вот тут нужно @username/chat_id вставить", "💩"))
